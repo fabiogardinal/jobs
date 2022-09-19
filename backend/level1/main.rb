7415c8f76619c74d03652a49864cc2f8df3a7035
Fabio Gardinal <fabio.gardinal@gmail.com>
2022-09-19 14:39:23 -0300

    started

---
+++ backend/level1/main.rb
@@ -0,0 +1,11 @@
+require 'json'
+require 'date'
+
+json = File.read('data/input.json')
+extract = JSON.parse(json)
+
+def time_converter(extract)
+#  converter o tempo no formato json
+end
+
+puts book_car(extract)
