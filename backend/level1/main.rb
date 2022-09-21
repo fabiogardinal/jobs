b4cfc295d3e6eda142c7e38fed886a86759ca102
Fabio Gardinal <fabio.gardinal@gmail.com>
2022-09-21 10:05:42 -0300

    return json and refactor code

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,41 +1,34 @@
 require 'json'
 require 'date'
 
+def calc_time(rentals, cars)
+  period = rentals.map do |rental|
+    s_date = Date.parse(rental['start_date'])
+    e_date = Date.parse(rental['end_date'])
+    (e_date - s_date).to_i
+  end
+  cars.zip(period).map { |car, days| car['price_per_day'] * days }
+end
+
+def calc_distance(rentals, cars)
+  distance = rentals.map { |rental| rental['distance'] }
+  cars.zip(distance).map { |car, km| car['price_per_km'] * km }
+end
+
+def json_converter(orders)
+  data = { rentals: [] }
+  orders.each_with_index { |order, i| data[:rentals] << { id: i + 1, price: order } }
+  data.to_json
+end
+
+def total_rent(rentals, cars)
+  total_distance = calc_distance(rentals, cars)
+  total_period = calc_time(rentals, cars)
+  orders = total_distance.zip(total_period).map { |km, days| km + days }
+  json_converter(orders)
+end
+
 json = File.read('data/input.json')
-extract = JSON.parse(json)
-@cars = extract['cars']
-@rentals = extract['rentals']
+data = JSON.parse(json)
 
-def time_converter
-  period = []
-  @rentals.each do |rental|
-    s_date = Date.parse rental['start_date']
-    e_date = Date.parse rental['end_date']
-    period << (e_date - s_date).to_i
-  end
-  @total_period = []
-  @cars.zip(period).each do |car, days|
-    @total_period << (car['price_per_day'] * days)
-  end
-  @total_period
-end
-
-def distance_converter
-  distance = []
-  @rentals.each do |rental|
-    distance << rental['distance']
-  end
-  @total_distance = []
-  @cars.zip(distance).each do |car, km|
-    @total_distance << (car['price_per_km'] * km)
-  end
-  @total_distance
-end
-
-def total_rent
-  @orders = []
-  @total_distance.zip(@total_period).each do |km, days|
-    @orders << (km + days)
-  end
-  @orders
-end
+total_rent(data['rentals'], data['cars'])
